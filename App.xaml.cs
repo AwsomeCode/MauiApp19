e594a7692a793d2600368c7ac33b2d545d9018f1
KalpeshLenovo <kalpeshchheda@outlook.com>
2024-11-29 11:06:06 +0530

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,79 @@
+﻿namespace MauiApp19
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new NavigationPage(new MainTabbedPage()));
+        }
+    }
+
+    public class MainTabbedPage : TabbedPage
+    {
+        public MainTabbedPage()
+        {
+            this.Children.Add(new ContentPage
+            {
+                Title = "Home Tab 1",
+                Content = new StackLayout
+                {
+                    Children = {
+                        new Label { Text = "Home Tab 1" },
+                        new Button { Text = "Go to ProductTabbedPage", 
+                            Command = new Command(Clicked) }
+                    }
+                }
+            });
+            this.Children.Add(new ContentPage
+            {
+                Title = "Home Tab 2",
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label { Text = "Home Tab 2" }
+                    }
+                }
+            });
+        }
+
+        private async void Clicked(object obj)
+        {
+            await Navigation.PushAsync(new ProductTabbedPage());
+        }
+    }
+
+    public class ProductTabbedPage : TabbedPage
+    {
+        public ProductTabbedPage()
+        {
+            this.Children.Add(new ContentPage
+            {
+                Title = "Product Tab 1",
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label { Text = "Product Tab 1" },
+                    }
+                }
+            });
+            this.Children.Add(new ContentPage
+            {
+                Title = "Product Tab 2",
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label { Text = "Product Tab 2" }
+                    }
+                }
+            });
+        }
+    }
+}
